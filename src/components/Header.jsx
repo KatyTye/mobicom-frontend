90848e6274375f4d8d99984e166eed109d49d8c3
David Majholt <d.boy.dragoz@gmail.com>
2026-03-05 13:10:10 +0100

    feat: added header with styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { PiDotsThreeVerticalBold, PiList } from "react-icons/pi";
+import { HiOutlineChevronLeft } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router";
+
+export default function Header() {
+	const [pageTitle, setPageTitle] = useState("");
+	const location = useLocation();
+
+	useEffect(() => {
+		if (location.pathname === "/") {
+			setPageTitle("Smart Home");
+		} else if (location.pathname === "/temp") {
+			setPageTitle("Varme");
+		} else if (location.pathname === "/stat") {
+			setPageTitle("Statistik");
+		} else {
+			setPageTitle("404");
+		}
+	}, [location.pathname]);
+
+	function returnLeftSideIcon() {
+		if (location.pathname === "/temp") {
+			return (
+				<Link to="/">
+					<HiOutlineChevronLeft className="top-content__back" />
+				</Link>
+			);
+		} else if (location.pathname === "/") {
+			return <PiList className="top-content__sidebar" />;
+		}
+
+		return <div></div>;
+	}
+
+	return (
+		<>
+			<header className="top-content">
+				{returnLeftSideIcon()}
+
+				<p className="top-content__title">{pageTitle}</p>
+
+				{location.pathname !== "/" ? (
+					<PiDotsThreeVerticalBold className="top-content__settings" />
+				) : (
+					<div></div>
+				)}
+			</header>
+
+			<div className="side-bar">
+				<p>Sidebar</p>
+			</div>
+		</>
+	);
+}
